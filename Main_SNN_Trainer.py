916c41fd34802cc873401736a9ab69d4d31395e0
dgutierrezATC <dgutierrez@atc.us.es>
2020-11-17 19:15:30 +0100

    Update code for making it generic

--- Main_SNN_Trainer.py
+++ Main_SNN_Trainer.py
@@ -30,49 +30,107 @@ import csv
 
 # If pyNAVIS was not installed from pip, clone the pyNAVIS repository
 # and use the following line:
-#from pyNAVIS import *
+from pyNAVIS import *
 
 # Otherwise, use this line:
-import pyNAVIS
+#import pyNAVIS
 
 #################################################################################
 # Configuration parameters
 #################################################################################
 
-#### NAS
-nas_num_freq_channels = 64
-nas_mono_stereo = 2
-nas_polarity_type = 2
+#### Training flags: WARNING: only one flag should be set to "True"
+is_nas2hidden_training = True
+is_hidden2output_training = False
 
-#### Recordings
-recordings_address_size = 4
-recordings_ts_tick = 1
-recordings_bin_size = 20000
+#### NAS layer
+nas_num_freq_channels = 64  # Number of freq. channels
+nas_mono_stereo = 2         # 1 = mono   ; 2 = stereo
+nas_polarity_type = 2       # 1 = single ; 2 = merged
+
+#### Hidden layer
+hidden_num_tones = 6
+
+#### Output layer
+output_num_tones = 6
 
 #### Tones
-num_tones = 6
+num_tones = hidden_num_tones
 
 training_tones_names = ["" for i in range(num_tones)]
 
-# Tone's names indicates their frequency (in Hz)
-training_tones_names[0] = "261"
+training_tones_names[0] = "261"  # Tone's names indicates their frequency (in Hz)
 training_tones_names[1] = "349"
 training_tones_names[2] = "523"
 training_tones_names[3] = "698"
 training_tones_names[4] = "1046"
 training_tones_names[5] = "1396"
 
-#### pyNAVIS 
-pynavis_settings = MainSettings(num_channels=64, mono_stereo=1, on_off_both=1, address_size=2, ts_tick=0.2, bin_size=20000)
+#### Recordings: NOTE: don't change here!
+recordings_num_channels = 0
+recordings_mono_stereo = 0
+recordings_on_off_both = 0
+recordings_address_size = 0
+recordings_ts_tick = 0.0
+recordings_bin_size = 0
 
-#### Output file
-output_weights_filename = "nas2toneshidden_weights_test.csv"
+#### Generic source-destination assigns: NOTE: don't change here!
+num_source_neurons = 0
+num_dest_neurons = 0
+training_tones_foldername = "training_tones\\"  # DON'T change this!
+output_weights_filename = ""
+
+# If we want to generate the weight files for the first projection
+if (is_nas2hidden_training == True):
+    # The number of input neurons will depend on the NAS configuration
+    # Note it does not matter if the NAS is stereo, because in that 
+    # case the activity of the right cochlea will be merged to
+    # the activity of the left cochlea
+    num_source_neurons = nas_num_freq_channels * nas_polarity_type
+    num_dest_neurons = hidden_num_tones
+
+    # The tones aedat recordings will be located in...
+    training_tones_foldername += "nas2toneshidden"
+    output_weights_filename = "nas2toneshidden_weights_test.csv"
+
+    # NOTE: check this before running. It depends on the used
+    # platform for performing the recordings
+    # Check https://pypi.org/project/pyNAVIS/ for the documentation
+    recordings_num_channels = nas_num_freq_channels
+    recordings_mono_stereo = nas_mono_stereo - 1
+    recordings_on_off_both = nas_polarity_type - 1
+    recordings_address_size = 2
+    recordings_ts_tick = 0.2
+    recordings_bin_size = 20000
+else:
+    # In this case, both source and destination have the same
+    # number of neurons.
+    num_source_neurons = hidden_num_tones
+    num_dest_neurons = output_num_tones
+
+    training_tones_foldername += "toneshidden2tonesoutput"
+    output_weights_filename = "toneshidden2tonesoutput_weights.csv"
+
+    # Because in this case the spikes are recorded from SpiNNaker,
+    # a .csv file will be used. This means no address size is needed.
+    # For the ts_tick, it's needed to set to a value which produces a
+    # resolution of microseconds
+    recordings_num_channels = hidden_num_tones
+    recordings_mono_stereo = 0
+    recordings_on_off_both = 0
+    recordings_address_size = 2
+    recordings_ts_tick = 0.001     # Converting from ms to us
+    recordings_bin_size = 20000
+
+
+#### pyNAVIS 
+pynavis_settings = MainSettings(num_channels=recordings_num_channels, mono_stereo=recordings_mono_stereo, on_off_both=recordings_on_off_both, address_size=recordings_address_size, ts_tick=recordings_ts_tick, bin_size=recordings_bin_size)
 
 #################################################################################
 # Code
 #################################################################################
 
-training_tones_foldername = "training_tones"
+# Get the absolute path to the folder that contains the recordings
 abs_path_training_tones_folder = os.path.dirname(__file__) + "\\" + training_tones_foldername
 
 # For each tone folder:
@@ -85,15 +143,19 @@ abs_path_training_tones_folder = os.path.dirname(__file__) + "\\" + training_ton
 #       |---> Save hisotram values in a file (this is weights files)
 
 # Create a list of list to save the histograms values
-training_tones_histograms = [[0.0 for i in range (nas_num_freq_channels*nas_polarity_type)] for i in range(num_tones)]
+training_tones_histograms = [[0.0 for i in range (num_source_neurons)] for i in range(num_dest_neurons)]
 
 # First, for each folder
 for folder_index in range (0, num_tones):
     # Go inside the folder
+    # First, we get the tone folder name
     current_training_tone_name = training_tones_names[folder_index]
+    # And create the tone folder path
     current_training_tone_folder_absolute_path =  abs_path_training_tones_folder + "\\" + current_training_tone_name + "\\"
+
     # Get a list with all the files inside the folder
     current_training_tone_filelist = os.listdir(current_training_tone_folder_absolute_path)
+    # Check how many elements there are inside
     num_files_current_training_tone_filelist = len(current_training_tone_filelist)
 
     print("Files found in folder " + str(current_training_tone_folder_absolute_path) + ": " )
@@ -109,51 +171,68 @@ for folder_index in range (0, num_tones):
         sample_filename_absolute_path = current_training_tone_folder_absolute_path + sample_filename
 
         # Load events file by usin pyNAVIS
-        sample_file_data = Loaders.loadAEDAT(sample_filename_absolute_path, pynavis_settings)
+        # If we are training the first projection group (nas2toneshidden), then use loadAEDAT
+        # Otherwise, use loadCSV (for reading events files from SpiNNaker or others)
+        if (is_nas2hidden_training == True):
+            sample_file_data = Loaders.loadAEDAT(sample_filename_absolute_path, pynavis_settings)
+        else:
+            sample_file_data = Loaders.loadCSV(sample_filename_absolute_path)
+        
+        # Then, call "pyNavis.adapt" for adapting the events' timestamps
         sample_file_data = Functions.adapt_SpikesFile(sample_file_data, pynavis_settings)
+        # And check that everything is OK
         Functions.check_SpikesFile(sample_file_data, pynavis_settings)
 
         # Calculate its histogram
         sample_file_histogram = Plots.histogram(sample_file_data, pynavis_settings)
+        # Convert the histogram type to float
         sample_file_histogram = sample_file_histogram.astype('float64')
 
-        # Check if it is a stereo NAS
-        if(nas_mono_stereo == 2):
+        # When nas2toneshidden flag is active, check if it is a stereo NAS
+        if((is_nas2hidden_training == True) and (nas_mono_stereo == 2)):
             # Sum both left and right histogram and calculate the mean
-            for channel_index in range(0, nas_num_freq_channels*nas_polarity_type):
+            for channel_index in range(0, num_source_neurons):
                 # Sum
-                sample_file_histogram[channel_index] += sample_file_histogram[channel_index + nas_num_freq_channels*nas_polarity_type]
+                sample_file_histogram[channel_index] += sample_file_histogram[channel_index + num_source_neurons]
                 # Div by 2
                 sample_file_histogram[channel_index] = float(sample_file_histogram[channel_index] / 2.0)
         
-        for channel_index in range(0, nas_num_freq_channels*nas_polarity_type):
-            training_tones_histograms[folder_index][channel_index] += sample_file_histogram[channel_index]
+        # Finally, we calculate the mean of the previous histogram with the new histogram
+        # If it is the first histogram, the mean is not calculated
+        for source_index in range(0, num_source_neurons):
+            training_tones_histograms[folder_index][source_index] += sample_file_histogram[source_index]
             if(file_index > 0):
-                training_tones_histograms[folder_index][channel_index] = float(training_tones_histograms[folder_index][channel_index] / 2.0)
+                training_tones_histograms[folder_index][source_index] = float(training_tones_histograms[folder_index][source_index] / 2.0)
+
+# Last step is to normalize the histogram for each pure tone according to the global activity
+# This means, we will normalize the firing activity of each channel with the firing activity
+# of the whole NAS for a especific tone.
 
 # Calculate the total number of events fired by the NAS for each tone
-max_num_events_per_tone = [0.0 for i in range(0, num_tones)]
+max_num_events_per_tone = [0.0 for i in range(0, num_dest_neurons)]
 
-for tone_index in range(0, num_tones):
+for tone_index in range(0, num_dest_neurons):
     total_activity = 0.0
-    for channel_index in range(0, nas_num_freq_channels*nas_polarity_type):
-        total_activity += training_tones_histograms[tone_index][channel_index]
+    for neuron_index in range(0, num_source_neurons):
+        # Calculate the global activity of the NAS for this tone
+        total_activity += training_tones_histograms[tone_index][neuron_index]
+    # Save the overall activity for this tone
     max_num_events_per_tone[tone_index] = total_activity
 
 # Normalize all the values
-training_tones_histograms_normalized = [[0.0 for i in range (nas_num_freq_channels*nas_polarity_type)] for i in range(num_tones)]
+training_tones_histograms_normalized = [[0.0 for i in range (num_source_neurons)] for i in range(num_dest_neurons)]
 
-for dest_index in range (0, num_tones):
-    for source_index in range(0, nas_num_freq_channels*nas_polarity_type):
+for dest_index in range (0, num_dest_neurons):
+    for source_index in range(0, num_source_neurons):
         training_tones_histograms_normalized[dest_index][source_index] = training_tones_histograms[dest_index][source_index] / max_num_events_per_tone[dest_index]
 
 # Finally, write a csv with the weights matrix
 with open(abs_path_training_tones_folder + '\\' + output_weights_filename, 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',')
     
-    for source_index in range(0, nas_num_freq_channels*nas_polarity_type):
-        row_to_write = ["" for i in range(0, num_tones)]
-        for dest_index in range(0, num_tones):
+    for source_index in range(0, num_source_neurons):
+        row_to_write = ["" for i in range(0, num_dest_neurons)]
+        for dest_index in range(0, num_dest_neurons):
             row_to_write[dest_index] = str(training_tones_histograms_normalized[dest_index][source_index])
         spamwriter.writerow(row_to_write)
         
